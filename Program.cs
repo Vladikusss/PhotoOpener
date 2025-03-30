da4a423e59633cc60a99ee3f0656f601780585ee
Vladdy <182237916+Vladikusss@users.noreply.github.com>
2025-03-30 18:36:02 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+ *
+ * 
+ */
+
+using System;
+using System.IO; // To read image files from disk
+using System.Net;
+using System.Net.Http; // To create local http server
+using System.Threading.Tasks; // For async operations
+
+
+
+
+
+namespace PhotoOpener;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# PhotoOpener
+ A simple program to open local photos on a local web-server in browser
